5a6fc30cac17a5971bd25699676d956ac96cff0a
MatthewLadas <ladas.matthew@gmail.com>
2022-03-16 18:58:14 -0400

    Practiced adding event listeners

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.querySelector('button').addEventListener('click', handleClick);
+
+// function handleClick() {
+// 	alert('I got clicked!');
+// }
+
+var numberOfDrums = document.querySelectorAll('.drum').length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+	document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+		alert('I got clicked!!');
+	});
+}
